23689ffd1f75acd0b8fab9ff61aa1c8026cb7050
Saranya <52063614+saranyakavins@users.noreply.github.com>
2023-12-03 11:04:12 +0530

    Update list-users.sh

--- GitHub_API/list-users.sh
+++ GitHub_API/list-users.sh
@@ -1,42 +1,63 @@
 #!/bin/bash
 
-# GitHub API URL
-API_URL="https://api.github.com"
+#########################################
+# Author: Saranya
+# Description: Script to fetch the owners of GitHub account
+#########################################
 
-# GitHub username and personal access token
-USERNAME=$username
-TOKEN=$token
+################################################
+# GitBub API documentation
+# https://docs.github.com/en/rest/collaborators/collaborators?apiVersion=2022-11-28
+# curl -L \
+# -H "Accept: application/vnd.github+json" \
+# -H "Authorization: Bearer <YOUR-TOKEN>" \
+# -H "X-GitHub-Api-Version: 2022-11-28" \
+#  https://api.github.com/repos/OWNER/REPO/collaborators
+#################################################
 
-# User and Repository information
-REPO_OWNER=$1
-REPO_NAME=$2
+# GitHub API Url
+api_url="https://api.github.com"
 
-# Function to make a GET request to the GitHub API
-function github_api_get {
-    local endpoint="$1"
-    local url="${API_URL}/${endpoint}"
+# GitHub Username and personal access token
+# Export username and token before executing this script
+username=$username
+token=$token
 
-    # Send a GET request to the GitHub API with authentication
-    curl -s -u "${USERNAME}:${TOKEN}" "$url"
+# GitHub User and Repository details
+ repo_owner=$1
+ repo_name=$2
+
+# Function to make a get request to GitHub API
+function get_github_api {
+local endpoint="$1"
+local url="${api_url}/${endpoint}"
+
+# send a GET request to GitHub API with authentication
+# -s -> silent or quiet mode, -u user:password
+curl -s -u "${username}:${token}" "$url"
 }
 
 # Function to list users with read access to the repository
 function list_users_with_read_access {
-    local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
+local endpoint="repos/${repo_owner}/${repo_name}/collaborators"
 
-    # Fetch the list of collaborators on the repository
-    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"
+# get the list of collaborators on the repository
+# jq -r command that extracts and outputs properly quoted strings for use by the shell
+# call the function get_github_api
+collabotorators="$(get_github_api "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"
 
-    # Display the list of collaborators with read access
-    if [[ -z "$collaborators" ]]; then
-        echo "No users with read access found for ${REPO_OWNER}/${REPO_NAME}."
-    else
-        echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
-        echo "$collaborators"
-    fi
+# display the list of collaborators with read access
+# -z -> string is null or zero length
+if [ -z "$collabotorators" ]; then
+echo "No users found with read access for ${repo_owner}/${repo_name}"
+else
+echo "Users with read access to ${repo_owner}/${repo_name}:"
+echo "$collabotorators"
+fi
 }
 
 # Main script
 
-echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
+echo "Listing users with read access to repo ${repo_owner}/${repo_name}: "
+#call the function list_users_with_read_access
 list_users_with_read_access
